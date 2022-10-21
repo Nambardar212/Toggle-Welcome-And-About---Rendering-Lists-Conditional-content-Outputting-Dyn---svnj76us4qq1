649d5995d8489f11e149785129740888e632a8b7
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-21 13:37:48 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [state, setState] = useState(true)
+  const handleToggle = () => {
+       if(state === true) {
+            setState(false)
+       } else {
+        setState(true)
+       }
+  }
   return (
     <div id="main">
       <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
+        {
+           state === true ? <h1>Welcome to Newton School!!</h1> : null
+        }
+        
       </div>
       <div id="about-div">
-        <h1>
+        {
+          state === false ? <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
-        </h1>
+        </h1> : null
+        }
       </div>
-      <button id="toggle">Toggle</button>
+      <button id="toggle" onClick={handleToggle}>Toggle</button>
     </div>
   );
 };
